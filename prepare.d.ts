df053acc1b5b2ecc8222c91e8515b111052a87ce
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-04-02 03:24:56 +0500

    fix(types): fix and improve typings

--- prepare.d.ts
+++ prepare.d.ts
@@ -13,3 +13,22 @@ export function prepare({
   binary: string;
   usePackageJson?: boolean;
 }): Promise<boolean>;
+
+export type maps = {
+  // Mapping constants
+  arch: {
+    arm64: "aarch64" | "arm64";
+    x86: "x86_64";
+    x64: "x86_64";
+  };
+  vendor: {
+    darwin: "apple";
+    win32: "pc";
+    linux: "unknown";
+  };
+  os: {
+    darwin: "darwin";
+    win32: "windows-msvc" | "windows";
+    linux: "linux" | "linux-musl";
+  };
+};
